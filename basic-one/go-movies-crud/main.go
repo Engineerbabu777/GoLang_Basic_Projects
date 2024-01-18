25acd17d3f96d4f229cc136130d344c931ec537e
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-18 16:12:44 +0500

    'movies-crud-basic'

--- basic-one/go-movies-crud/main.go
+++ basic-one/go-movies-crud/main.go
@@ -32,9 +32,58 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movies);
 }
 
-func getMovie(w http.ResponseWriter, r *http.Request){}
-func createMovie(w http.ResponseWriter, r *http.Request){}
-func updateMovie(w http.ResponseWriter, r *http.Request){}
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json");
+	params := mux.Vars(r);
+	for _, item := range movies{
+       if(item.ID == params["id"]){
+		json.NewEncoder(w).Encode(item);
+		return
+	   }
+	}
+
+}
+func createMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json");
+	var movie Movie;
+	_ = json.NewDecoder(r.Body).Decode(&movie);
+	movie.ID = strconv.Itoa(rand.Intn(10000000));
+	movies = append(movies, movie);
+	json.NewEncoder(w).Encode(movie);
+}
+
+// Define a function named updateMovie that handles an HTTP request for updating a movie.
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// Set the Content-Type header of the response to indicate JSON content.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Get the parameters from the request using mux.Vars(r).
+	params := mux.Vars(r)
+
+	// Loop through the movies to find and update the movie with the specified ID.
+	for index, item := range movies {
+		// Check if the ID of the current movie matches the ID from the request parameters.
+		if item.ID == params["id"] {
+			// If a match is found, remove the movie from the movies slice.
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+
+	// Decode the JSON data from the request body into a new movie object.
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	// Set the ID of the movie to the ID from the request parameters.
+	movie.ID = params["id"]
+
+	// Append the updated movie to the movies slice.
+	movies = append(movies, movie)
+
+	// Encode the updated movie in JSON format and send it as the HTTP response.
+	json.NewEncoder(w).Encode(movie)
+}
+
 
 func deleteMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-type","application/json");
@@ -42,8 +91,10 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 	for index, item := range movies{
 		if item.ID == params["id"]{
 			movies = append(movies[:index], movies[index+1:]...);
+			break;
 		}
 	}
+	json.NewEncoder(w).Encode(movies);
 }
 
 func main(){
